f4c14688d6f7d1eaad97b4fe53ffc0d8f8533272
AKen00 <ajken04@gmail.com>
2020-09-06 12:12:40 -0400

    redoing timer and color functions

--- Calender-App/Develop/script.js
+++ Calender-App/Develop/script.js
@@ -1,56 +1,36 @@
-// Date and time code
-var tasks = $(".selected-hour")
-var currentDay = $("#currentDay");
-var currentTimer = $("#timer");
+$(document).ready(function(){
+
+// Moment.js date and time code variables 
+    var tasks = moment().hours();
+
+    function getDate(){
+        $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    };
+
+// // each time block is color-coded to indicate whether it is in the past, present, or future
+    function hourColor(){
+        $("textarea").each(function(){
+            var blockHour = $(this).attr("id")
+            var blockNumber =parseInt(blockHour);
+            if (blockNumber === hour){
+                $(this).addClass("present");
+            } else if (blockNumber < hour){
+                $(this).addClass("past");
+            } else {
+                $(this).addClass("future");
+            };
+        });
+    };
 
 
 
-console.log(tasks);
 
-// each time block is color-coded to indicate whether it is in the past, present, or future
-var hourColor = function() {
-    var currentHour = (moment().hour());
+// // Date and time code variables
 
-    for (var i = 0; i < tasks.length; i++) {
-        var task = $(tasks[i])
-        var blockHour = task.data("hour");
+// // Save button for saving to local storage
 
+// // Click into a time block and enter an event
 
-        if (blockHour < currentHour) {
-            task
-            .addClass("past")
-        } 
-        else if (task === currentHour) {
-            task
-            .addClass("present")
-        }
-        else {
-            task
-            .addClass("future")
-    }
-}
-};
+// // The text for that event is saved in local storage
 
-var updateTime = function() {
-
-    var timeNow = moment();
-    var displayTime = timeNow.format("LL");
-    currentDay.text(displayTime);
-};
-
-var clocks = function() {
-    var timeNow =moment();
-
-    var clockDisplay = timeNow.format("LTS")
-
-}
-
-
-// Save button for saving to local storage
-// var tasks = JSON.parse(localStorage.getItem('text-area')) || [];
-
-// Click into a time block and enter an event
-
-// The text for that event is saved in local storage
-
-// The saved events persist
+// // The saved events persist.
